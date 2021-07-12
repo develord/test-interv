1e760e858d78076d76fab8104212409111ed8f74
aminefr <mohamed.amine.fredj@gmail.com>
2021-07-12 14:22:47 +0100

    handel error

--- src/components/treeFolder.vue
+++ src/components/treeFolder.vue
@@ -1,6 +1,11 @@
 <template>
+  <template v-if="loader">
+    loading ...
+  </template>
+  <template v-else>
       <userCard  v-if="!folder.children || level === 1"  :level="level"   :data="folder" />
 			<treeContents :children="folder" :level="level + 1"></treeContents>
+  </template>
 </template>
 
 <script>
@@ -14,6 +19,21 @@ export default {
     level: {
       type: Number,
       default: 1
+    }
+  },
+  data() {
+    return {
+      loader: true
+    }
+  },
+  watch: {
+    folder: {
+      immediate: true,
+      handler(newVal) {
+       if (newVal) {
+         this.loader = false
+       } 
+      }
     }
   }
 }
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="home">
-    <treeFolder :folder="treeData"/>
+    <treeFolder v-if="!msgError" :folder="treeData"/>
+    <p v-else v-html="msgError" />
   </div>
 </template>
 <script>
@@ -9,83 +10,30 @@ export default {
   name: 'Home',
   data() {
     return {
-      treeData: {
-        name: "Naruto uzumaki",
-        title: "CTO",
-        img: "https://iconape.com/wp-content/files/hb/154859/svg/154859.svg",
-        children: [
-          {
-            name: "Luffy",
-            title: "Directeur Developpement",
-            img: "https://icons.iconarchive.com/icons/crountch/one-piece-character/256/Monkey-D-Luffey-icon.png",
-            children: [
-              { 
-                name: "Donald trump",
-                title: "chef Equipe",
-                img: "https://webmedias.ac-nice.fr/chasseursdactu/wp-content/uploads/sites/39/2018/03/trump-800x445.png",
-                children: [
-                  { 
-                    name: "buggy ",
-                    title: "Developpeur",
-                    img: "https://i.skyrock.net/2767/78482767/pics/3002856667_1_17_zt0vAVxu.png",
-                  },
-                  { 
-                    name: "Gattuso",
-                    title: "Developpeur",
-                    img: "https://www.footyrenders.com/render/gennaro-gattuso-1.png"
-                  },
-                  { 
-                    name: "Pepe",
-                    title: "Developpeur",
-                    img: "http://www.footugalfrance.fr/media/bearleague/thumb/bl15676139172440.png"
-                  }
-                ] 
-              },
-              { 
-                name: "Kim Jong Un",
-                title: "chef Equipe",
-                img: "https://s3.getstickerpack.com/storage/uploads/sticker-pack/great-leader-kim-jong-un/sticker_3.png?d0206403c0cca86b656d68031fa585e1&d=200x200",
-                children: [
-                  { 
-                    name: "Saitama",
-                    title: "Developpeur",
-                    img: "https://www.pngkey.com/png/full/146-1460159_saitama-sticker-one-punch-man-saitama-ok.png",
-                  },
-                  { 
-                    name: "Goku",
-                    title: "Developpeur",
-                    img: "https://i.pinimg.com/originals/d5/00/47/d50047b6bdfc27e96f6b9ffb40a25188.png"
-                  },
-                  { 
-                    name: "Madara",
-                    title: "Developpeur",
-                    img: "https://i.pinimg.com/originals/f2/fe/c2/f2fec265ef3f424cacefd43816ea65ad.png"
-                  }
-                ] 
-              }
-            ]
-          },
-          {
-            name: "Salt Bae",
-            title: "Directeur Deployment",
-            img: "https://emoji.gg/assets/emoji/2199-saltbae.png",
-            children: [
-              {
-                name: "Elon Musk",
-                title: "chef Equipe",
-                img: "http://csc170.org/examples/gilacqua/project3/images/elon1.png",
-              },
-              {
-                name: "Pep Guardiola",
-                title: "chef Equipe",
-                img: "https://i.4pcdn.org/sp/1537627538858.png",
-              }
-            ]
-          }
-        ]
-      }
+      treeData: null,
+      msgError: null,
     }
   },
+  methods: {
+    async getOrganization () {
+        let url = `${process.env.VUE_APP_SERVER_PATH}oraganigram`
+        let res = await fetch(url)
+        if (res.ok) {
+            let text = await res.text()
+            return {data: text, status: true}
+        } else {
+            return  {data: `HTTP error: ${res.status}`, status: false}
+        }
+    }
+  },
+  async created() {
+    const {data, status} = await this.getOrganization()
+    if (status) {
+      this.treeData = data
+    } else {
+      this.msgError = data
+    }
+  }
 }
 </script>
 <style>
